ec5be49fbb8cd20eedea408dc81df1e32e68d85f
Artyom Pugachev <artempugachev1@gmail.com>
2022-04-13 16:06:52 +0300

    init

---
+++ hello/frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Nearspring Hello</h1>
+      </header>
+
+      <body>
+
+      </body>
+    </div>
+  );
+}
+
+export default App;
