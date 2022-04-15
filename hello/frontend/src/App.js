35e8ba9e945f7530b95046928b22ec9ba7d59695
Artyom Pugachev <artempugachev1@gmail.com>
2022-04-15 17:53:48 +0300

    input form

--- hello/frontend/src/App.js
+++ hello/frontend/src/App.js
@@ -1,20 +1,39 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, {useState, useEffect} from 'react';
-import { Container, Row, Col, ListGroup, Placeholder } from 'react-bootstrap';
+import { Form, InputGroup, FormControl, Button, Placeholder } from 'react-bootstrap';
 
 
 import './App.css';
 
 function App() {
+  const [greetingInput, setGreetingInput] = useState("")
+
+  const handleSubmit = () => {
+    console.log(greetingInput);
+  }
+
   return (
     <div className="app">
       <header className="app-header">
         <h1>Nearspring Hello</h1>
       </header>
 
-      <body>
-
-      </body>
+      <main>
+        <Form onSubmit={(event) => {event.preventDefault(); handleSubmit()}}>
+          <InputGroup className="greeting-input">
+            <FormControl
+              placeholder="Your name"
+              aria-label=""
+              aria-describedby=""
+              onChange={(event) => {setGreetingInput(event.target.value)}}
+            />
+            
+            <Button type="submit" variant="primary" id="hello-button">
+              Hello
+            </Button>
+          </InputGroup>
+        </Form>
+      </main>
     </div>
   );
 }
