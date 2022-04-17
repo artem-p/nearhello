c2af2160bf53c42895c6f61bb9b41338ef180c23
Artyom Pugachev <artempugachev1@gmail.com>
2022-04-17 16:23:05 +0300

    redundant methods

--- hello/contract/src/lib.rs
+++ hello/contract/src/lib.rs
@@ -1,6 +1,5 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{env, near_bindgen};
-use near_sdk::collections::LookupMap;
 
 near_sdk::setup_alloc!();
 
@@ -8,98 +7,23 @@ near_sdk::setup_alloc!();
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Hello {
-    greetings: LookupMap<String, String>,
 }
 
 
 impl Default for Hello {
     fn default() -> Self {
         Self {
-            greetings: LookupMap::new(b"a".to_vec()),
         }
     }
 }
 
 #[near_bindgen]
 impl Hello {
-    pub fn set_greeting(&mut self, greeting: String) {
-        let account_id = env::signer_account_id();
-
-        self.greetings.insert(&account_id, &greeting);
-    }
-
-    pub fn get_greeting(&self, account_id: String) -> String {
-        match self.greetings.get(&account_id) {
-            Some(greeting) => greeting,
-            None => "Hello".to_string(),
-        }
+    pub fn hello(&self, greeting: String) -> String {
+        return "Hello ".to_owned() + &greeting;
     }
 }
 
-
-// #[near_bindgen]
-// impl Counter {
-//     /// Returns 8-bit signed integer of the counter value.
-//     ///
-//     /// This must match the type from our struct's 'val' defined above.
-//     ///
-//     /// Note, the parameter is `&self` (without being mutable) meaning it doesn't modify state.
-//     /// In the frontend (/src/main.js) this is added to the "viewMethods" array
-//     /// using near-cli we can call this by:
-//     ///
-//     /// ```bash
-//     /// near view counter.YOU.testnet get_num
-//     /// ```
-//     pub fn get_num(&self) -> i8 {
-//         return self.val;
-//     }
-
-//     /// Increment the counter.
-//     ///
-//     /// Note, the parameter is "&mut self" as this function modifies state.
-//     /// In the frontend (/src/main.js) this is added to the "changeMethods" array
-//     /// using near-cli we can call this by:
-//     ///
-//     /// ```bash
-//     /// near call counter.YOU.testnet increment --accountId donation.YOU.testnet
-//     /// ```
-//     pub fn increment(&mut self) {
-//         // note: adding one like this is an easy way to accidentally overflow
-//         // real smart contracts will want to have safety checks
-//         // e.g. self.val = i8::wrapping_add(self.val, 1);
-//         // https://doc.rust-lang.org/std/primitive.i8.html#method.wrapping_add
-//         self.val += 1;
-//         let log_message = format!("Increased number to {}", self.val);
-//         env::log(log_message.as_bytes());
-//         after_counter_change();
-//     }
-
-//     /// Decrement (subtract from) the counter.
-//     ///
-//     /// In (/src/main.js) this is also added to the "changeMethods" array
-//     /// using near-cli we can call this by:
-//     ///
-//     /// ```bash
-//     /// near call counter.YOU.testnet decrement --accountId donation.YOU.testnet
-//     /// ```
-//     pub fn decrement(&mut self) {
-//         // note: subtracting one like this is an easy way to accidentally overflow
-//         // real smart contracts will want to have safety checks
-//         // e.g. self.val = i8::wrapping_sub(self.val, 1);
-//         // https://doc.rust-lang.org/std/primitive.i8.html#method.wrapping_sub
-//         self.val -= 1;
-//         let log_message = format!("Decreased number to {}", self.val);
-//         env::log(log_message.as_bytes());
-//         after_counter_change();
-//     }
-
-//     /// Reset to zero.
-//     pub fn reset(&mut self) {
-//         self.val = 0;
-//         // Another way to log is to cast a string into bytes, hence "b" below:
-//         env::log(b"Reset counter to zero");
-//     }
-// }
 
 // // unlike the struct's functions above, this function cannot use attributes #[derive(…)] or #[near_bindgen]
 // // any attempts will throw helpful warnings upon 'cargo build'
