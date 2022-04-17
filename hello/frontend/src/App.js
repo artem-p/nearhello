2afd0dcf25f2dba12c9bf1ae45da75106e41de4c
Artyom Pugachev <artempugachev1@gmail.com>
2022-04-17 16:53:46 +0300

    call api

--- hello/frontend/src/App.js
+++ hello/frontend/src/App.js
@@ -25,40 +25,65 @@ function App() {
 
   const handleSubmit = () => {
     console.log(greetingInput);
+
+    fetchGreeting();
   }
 
-  const fetchGreeting = () => {
+  async function fetchGreeting() {
+    near = await connect(testnet_config);
 
+    const account = await near.account('nearspring-hello1.artyom-p.testnet')
+    
+    const contract = await new nearApi.Contract(account, 'nearspring-hello1.artyom-p.testnet', {
+      viewMethods: ["hello"],
+    })
+
+    const response = await contract.hello({greeting: "Artyom"})
+
+    console.log(response)
+
+    // const getGreetingRawResult = await near.connection.provider.query({
+    //   request_type: "call_function",
+    //   account_id: 'nearspring-hello1.artyom-p.testnet',
+    //   method_name: 'hello',
+    //   args_base64: "e30=QXJ0eW9t",
+    //   finality: "optimistic",
+    // });
+
+    // console.log(getGreetingRawResult);
+
+    // const formattedResult = JSON.parse(Buffer.from(getGreetingRawResult.result).toString());
+    // console.log(formattedResult);
   }
 
 
-  useEffect(() => {
-    async function fetchInfo() {
+  // useEffect(() => {
+  //   async function fetchInfo() {
 
-      near = await connect(testnet_config);
+  //     near = await connect(testnet_config);
 
-      try {
-        const getGreetingRawResult = await near.connection.provider.query({
-          request_type: "call_function",
-          account_id: 'nearspring-hello.artyom-p.testnet',
-          method_name: 'get_greeting',
-          args_base64: "e30=",
-          finality: "optimistic",
-      });
+  //     try {
+  //       const getGreetingRawResult = await near.connection.provider.query({
+  //         request_type: "call_function",
+  //         account_id: 'nearspring-hello.artyom-p.testnet',
+  //         method_name: 'get_greeting',
+  //         args_base64: "e30=",
+  //         finality: "optimistic",
+  //     });
 
-      console.log(getGreetingRawResult);
+  //     console.log(getGreetingRawResult);
 
-      const formattedResult = JSON.parse(Buffer.from(getGreetingRawResult.result).toString());
-      console.log(formattedResult);
+  //     const formattedResult = JSON.parse(Buffer.from(getGreetingRawResult.result).toString());
+  //     console.log(formattedResult);
       
-      } catch (error) {
-          console.log(error)
-          return;
-      }
-    }
+  //     } catch (error) {
+  //         console.log(error)
+  //         return;
+  //     }
+  //   }
 
-    fetchInfo()
-  }, [])
+  //   fetchInfo()
+  // }, [])
 
   const Hello = () => {
     if (greetingFromBlockchain) {
